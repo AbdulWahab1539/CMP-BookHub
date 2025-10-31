c4cfbc49c20edf6d1b631af43c3d24f973560f32
Abdul Wahab <abdul.wahab@9dtechnologies.com>
2025-10-31 17:43:38 +0500

    refactor: update navigation structure, add error messages, enhance search flow, and improve dependency injection setup across platforms, completed Book List Screen

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/TitledContent.kt
@@ -0,0 +1,18 @@
+package com.plcoding.bookpedia.book.presentation.book_detail.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TitledContent(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+fun TitledContentPrev() {
+    TitledContent()
+}
